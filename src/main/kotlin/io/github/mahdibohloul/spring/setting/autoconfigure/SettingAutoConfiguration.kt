8e89febfbc65c1698fd3238256aeab6112cc30e6
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-10-14 10:44:36 +0330

    Add `SettingAutoConfiguration` for Spring Boot autoconfiguration
    Introduce `SettingAutoConfiguration` to auto-configure `SettingReader`, `SettingWriter`, `SettingRepository`, and `SettingService` beans conditionally, with support for Jackson-based serialization and in-memory repository setup.

---
+++ src/main/kotlin/io/github/mahdibohloul/spring/setting/autoconfigure/SettingAutoConfiguration.kt
@@ -0,0 +1,32 @@
+package io.github.mahdibohloul.spring.setting.autoconfigure
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.mahdibohloul.spring.setting.reader.SettingReader
+import io.github.mahdibohloul.spring.setting.reader.SettingReaderImpl
+import io.github.mahdibohloul.spring.setting.repositories.SettingRepository
+import io.github.mahdibohloul.spring.setting.repositories.SimpleInMemorySettingRepository
+import io.github.mahdibohloul.spring.setting.services.SettingService
+import io.github.mahdibohloul.spring.setting.services.SettingServiceImpl
+import io.github.mahdibohloul.spring.setting.writer.SettingWriter
+import io.github.mahdibohloul.spring.setting.writer.SettingWriterImpl
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+
+@AutoConfiguration
+class SettingAutoConfiguration {
+  @ConditionalOnMissingBean(SettingWriter::class)
+  @ConditionalOnBean(ObjectMapper::class)
+  fun settingWriter(objectMapper: ObjectMapper): SettingWriter = SettingWriterImpl(objectMapper)
+
+  @ConditionalOnMissingBean(SettingReader::class)
+  @ConditionalOnBean(ObjectMapper::class)
+  fun settingReader(objectMapper: ObjectMapper): SettingReader = SettingReaderImpl(objectMapper)
+
+  @ConditionalOnMissingBean(SettingService::class)
+  @ConditionalOnBean(SettingRepository::class)
+  fun settingService(settingRepository: SettingRepository): SettingService = SettingServiceImpl(settingRepository)
+
+  @ConditionalOnMissingBean(SettingRepository::class)
+  fun settingRepository(): SettingRepository = SimpleInMemorySettingRepository()
+}
