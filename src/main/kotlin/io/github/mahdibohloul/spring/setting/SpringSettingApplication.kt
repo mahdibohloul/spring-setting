76a90ea22263132e1a1082087c71c9c11f0e6854
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-10-14 09:50:37 +0330

    Initialize Spring Boot project with Kotlin support. Add Gradle wrapper, Kotlin DSL configuration, and initial application setup files.

---
+++ src/main/kotlin/io/github/mahdibohloul/spring/setting/SpringSettingApplication.kt
@@ -0,0 +1,11 @@
+package io.github.mahdibohloul.spring.setting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSettingApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringSettingApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/mahdibohloul/spring/setting/SpringSettingApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mahdibohloul.spring.setting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSettingApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
